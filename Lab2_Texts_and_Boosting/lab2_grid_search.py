2c7fb778bb9c53bf6e836971386f33bdc9e05092
Shiyanov Vadim <vadimsh853@gmail.com>
2019-05-04 20:14:37 +0000

    Leave only catboost

--- Lab2_Texts_and_Boosting/lab2_grid_search.py
+++ Lab2_Texts_and_Boosting/lab2_grid_search.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from catboost import CatBoostClassifier
-from lightgbm import LGBMClassifier
 from sklearn.decomposition import PCA
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 
@@ -32,22 +31,13 @@ if __name__ == '__main__':
     X_train_pca = pca.fit_transform(X_train_unique)
     X_test_pca = pca.transform(X_test_unique)
 
-    lgbm_params = {
-        'max_depth': np.append(-1, np.arange(1, 11)),
-        'n_estimators': np.append(np.arange(100, 1100, 100), np.arange(1500, 5000, 500)),
-    }
-    lgbm_search = GridSearchCV(estimator=LGBMClassifier(random_state=42), 
-                               param_grid=lgbm_params, 
-                               cv=StratifiedKFold(n_splits=5)).fit(X_train_pca, y_train)
-    lgbm_results = pd.DataFrame(lgbm_search.cv_results_)
-    lgbm_results.to_csv('./lgbm_results.csv')
-
-    catboost_params = {
+    params = {
         'depth': np.arange(1, 11),
         'iterations': np.append(np.arange(100, 1100, 100), np.arange(1500, 5000, 500)),
     }
-    catboost_search = GridSearchCV(estimator=CatBoostClassifier(random_state=42), 
-                                   param_grid=catboost_params, 
-                                   cv=StratifiedKFold(n_splits=5)).fit(X_train_pca, y_train)
-    catboost_results = pd.DataFrame(catboost_search.cv_results_)
-    catboost_results.to_csv('./catboost_results.csv')
+    grid_search = GridSearchCV(estimator=CatBoostClassifier(random_state=42, loss_function='MultiClass'),
+                               scoring='accuracy',
+                               param_grid=params, 
+                               cv=StratifiedKFold(n_splits=5)).fit(X_train_pca, y_train, verbose=False)
+    grid_res = pd.DataFrame(grid_search.cv_results_)
+    grid_res.to_csv('./grid_res.csv')
